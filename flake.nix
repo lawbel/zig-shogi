c3c188a3d3f3a4d76aee5d449565f68f84cd241d
Lawrence Bell <lawrence.matthew.bell@gmail.com>
2023-12-29 23:01:04 +0000

    add ZLS as nix input

--- flake.nix
+++ flake.nix
@@ -1,8 +1,10 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
+
     flake-utils.url = "github:numtide/flake-utils";
     flake-compat.url = "github:edolstra/flake-compat";
+
     zig = {
       url = "github:mitchellh/zig-overlay";
       inputs.flake-utils.follows = "flake-utils";
@@ -10,9 +12,15 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    zls = {
+      url = "github:zigtools/zls";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.zig-overlay.follows = "zig";
+    };
   };
 
-  outputs = { self, nixpkgs, zig, flake-utils, ... }:
+  outputs = { self, nixpkgs, zig, zls, flake-utils, ... }:
     let
       zig-pkgs = final: prev: {
         zigpkgs = zig.packages.${prev.system};
@@ -25,7 +33,10 @@
           pkgs = import nixpkgs { inherit overlays system; };
         in {
           devShells.default = pkgs.mkShell {
-            nativeBuildInputs = [ pkgs.zigpkgs.master ];
+            nativeBuildInputs = [
+              pkgs.zigpkgs.master
+              zls.packages.${system}.zls
+            ];
           };
         }
       );
