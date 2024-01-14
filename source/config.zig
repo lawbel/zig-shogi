b67f65a18303912377d105f17ec4d37b04e0b6a9
Lawrence Bell <lawrence.matthew.bell@gmail.com>
2024-01-14 00:24:07 +0000

    tweak board size

--- source/config.zig
+++ source/config.zig
@@ -2,11 +2,13 @@ const c = @cImport({
     @cInclude("SDL2/SDL.h");
     @cInclude("SDL2/SDL_image.h");
 });
+const ty = @import("types.zig");
 
 pub const window_title: [:0]const u8 = "Zig Shogi";
 
-pub const window_width: c_int = 600;
-pub const window_height: c_int = 600;
+pub const tile_size: c_int = 70;
+pub const window_width: c_int = tile_size * ty.Board.size;
+pub const window_height: c_int = tile_size * ty.Board.size;
 
 pub const window_flags: u32 = 0;
 
