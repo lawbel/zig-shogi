e2901527108675c95e0d41fe1c274d8d0171a0f9
lawbel <lawbel@hotmail.co.uk>
2024-02-12 00:10:07 +0000

    handle new board coordinates

--- source/pixel.zig
+++ source/pixel.zig
@@ -24,28 +24,47 @@ pub const PixelPos = struct {
     x: i32,
     y: i32,
 
+    /// Add these coordinates together.
+    pub fn add(this: @This(), other: @This()) @This() {
+        return .{
+            this.x + other.x,
+            this.y + other.y,
+        };
+    }
+
+    /// Subtract the second coordinate from the
+    /// first: `a.subtract(b) == a - b`.
+    pub fn subtract(this: @This(), other: @This()) @This() {
+        return .{
+            this.x - other.x,
+            this.y - other.y,
+        };
+    }
+
     /// Returns the offset of this position from the board grid. For example,
     /// if this position is right in the middle of a tile, it would return
     /// `(tile_size / 2, tile_size / 2)`.
     pub fn offsetFromGrid(this: @This()) @This() {
+        const from_top_left = this.subtract(board_top_left);
         return .{
-            .x = @mod(this.x, tile_size),
-            .y = @mod(this.y, tile_size),
+            .x = @mod(from_top_left.x, tile_size),
+            .y = @mod(from_top_left.y, tile_size),
         };
     }
 
     /// Returns the position on the board at this location on the screen.
-    pub fn toBoardPos(this: @This()) model.BoardPos {
+    pub fn toBoardPos(this: @This()) ?model.BoardPos {
+        if (!this.isOnTheBoard()) return null;
+
+        const from_top_left = this.subtract(board_top_left);
         return .{
-            .x = @intCast(@divFloor(this.x, tile_size)),
-            .y = @intCast(@divFloor(this.y, tile_size)),
+            .x = @intCast(@divFloor(from_top_left.x, tile_size)),
+            .y = @intCast(@divFloor(from_top_left.y, tile_size)),
         };
     }
 
     /// Whether or not these coordinates are within the bounds of the board.
     pub fn isOnTheBoard(this: @This()) bool {
-        const board_size_pix = tile_size * model.Board.size;
-
         const x_on_board =
             board_top_left.x <= this.x and
             this.x < board_top_left.x + board_size_pix;
@@ -63,16 +82,21 @@ pub const board_top_left: PixelPos = .{
     .y = 35,
 };
 
+/// The size of the board (width/height), in pixels.
+const board_size_pix: c_int = tile_size * model.Board.size;
+
 test "PixelPos.toBoardPos(n*size, n*size) returns (n, n)" {
     for (0..model.Board.size) |n| {
         const n_float: f32 = @floatFromInt(n);
         const tile_size_float: f32 = @floatFromInt(tile_size);
 
         // A pixel dead-centre in the middle of the intended tile.
-        const pix: PixelPos = .{
+        const base_pix: PixelPos = .{
             .x = @intFromFloat((n_float + 0.5) * tile_size_float),
             .y = @intFromFloat((n_float + 0.5) * tile_size_float),
         };
+        const pix = base_pix.add(board_top_left);
+
         const pos: model.BoardPos = .{
             .x = @intCast(n),
             .y = @intCast(n),
